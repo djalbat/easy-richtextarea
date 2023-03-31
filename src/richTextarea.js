5aa8aaae5268698e5fd7039b5882ca30d9aea5fb
James Smith <james.smith@djalbat.com>
2023-03-31 17:09:22 +0100

    Added some vacuous handlers to take the load off the pretty printer.

--- src/richTextarea.js
+++ src/richTextarea.js
@@ -6,17 +6,18 @@ import Selection from "./selection"
 import eventMixins from "./mixins/event";
 
 const { BLUR_EVENT_TYPE,
-        INPUT_EVENT_TYPE,
         FOCUS_EVENT_TYPE,
-        CHANGE_EVENT_TYPE,
+        INPUT_EVENT_TYPE,
         SCROLL_EVENT_TYPE,
+        CHANGE_EVENT_TYPE,
+        CUT_EVENT_TYPE,
+        COPY_EVENT_TYPE,
+        PASTE_EVENT_TYPE,
+        KEYDOWN_EVENT_TYPE,
+        MOUSEDOWN_EVENT_TYPE,
         SELECTIONCHANGE_EVENT_TYPE } = eventTypes;
 
 export default class RichTextarea extends Element {
-  inputHandler = (event, element) => {
-    this.intermediateHandler(event, element);
-  }
-
   blurHandler = (event, element) => {
     const eventType = BLUR_EVENT_TYPE,
           selectionChanged = true;
@@ -33,14 +34,34 @@ export default class RichTextarea extends Element {
     });
   }
 
+  inputHandler = (event, element) => {
+    this.intermediateHandler(event, element);
+  }
+
   scrollHandler = (event, element) => {
     const eventType = SCROLL_EVENT_TYPE;
 
     this.callHandlers(eventType, event, element);
   }
 
-  contextmenuHandler = (event, element) => {
-    event.preventDefault();
+  cutHandler = (event, element) => {
+    ///
+  }
+
+  copyHandler = (event, element) => {
+    ///
+  }
+
+  pasteHandler = (event, element) => {
+    ///
+  }
+
+  keyDownHandler = (event, element) => {
+    ///
+  }
+
+  mouseDownHandler = (event, element) => {
+    ///
   }
 
   selectChangeHandler = (event, element) => {
@@ -78,17 +99,22 @@ export default class RichTextarea extends Element {
     eventListeners.forEach((eventListener) => {
       let { element } = eventListener;
 
-      const { handler } = eventListener,
-            handlerElement = element; ///
+      const { handler } = eventListener;
 
-      element = this; ///
-
-      if ( (handler !== this.inputHandler)
-        && (handler !== this.blurHandler)
+      if ( (handler !== this.blurHandler)
         && (handler !== this.focusHandler)
+        && (handler !== this.inputHandler)
         && (handler !== this.scrollHandler)
-        && (handler !== this.contextmenuHandler)
+        && (handler !== this.cutHandler)
+        && (handler !== this.copyHandler)
+        && (handler !== this.pasteHandler)
+        && (handler !== this.keyDownHandler)
+        && (handler !== this.mouseDownHandler)
         && (handler !== this.selectChangeHandler) ) {
+
+        const handlerElement = element; ///
+
+        element = this; ///
 
         handler.call(handlerElement, ...remainingArguments, element);
       }
@@ -193,13 +219,23 @@ export default class RichTextarea extends Element {
   }
 
   activate() {
-    this.on(INPUT_EVENT_TYPE, this.inputHandler);
-
     this.on(BLUR_EVENT_TYPE, this.blurHandler);
 
     this.on(FOCUS_EVENT_TYPE, this.focusHandler);
 
+    this.on(INPUT_EVENT_TYPE, this.inputHandler);
+
     this.on(SCROLL_EVENT_TYPE, this.scrollHandler);
+
+    this.on(CUT_EVENT_TYPE, this.cutHandler);
+
+    this.on(COPY_EVENT_TYPE, this.copyHandler);
+
+    this.on(PASTE_EVENT_TYPE, this.pasteHandler);
+
+    this.on(KEYDOWN_EVENT_TYPE, this.keyDownHandler);
+
+    this.on(MOUSEDOWN_EVENT_TYPE, this.mouseDownHandler);
 
     document.on(SELECTIONCHANGE_EVENT_TYPE, this.selectChangeHandler);
 
@@ -207,13 +243,23 @@ export default class RichTextarea extends Element {
   }
 
   deactivate() {
-    this.off(INPUT_EVENT_TYPE, this.inputHandler);
-
     this.off(BLUR_EVENT_TYPE, this.blurHandler);
 
     this.off(FOCUS_EVENT_TYPE, this.focusHandler);
 
+    this.off(INPUT_EVENT_TYPE, this.inputHandler);
+
     this.off(SCROLL_EVENT_TYPE, this.scrollHandler);
+
+    this.off(CUT_EVENT_TYPE, this.cutHandler);
+
+    this.off(COPY_EVENT_TYPE, this.copyHandler);
+
+    this.off(PASTE_EVENT_TYPE, this.pasteHandler);
+
+    this.off(KEYDOWN_EVENT_TYPE, this.keyDownHandler);
+
+    this.off(MOUSEDOWN_EVENT_TYPE, this.mouseDownHandler);
 
     document.off(SELECTIONCHANGE_EVENT_TYPE, this.selectChangeHandler);
 
