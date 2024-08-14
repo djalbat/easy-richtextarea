e49cf181250c36f18c03051f6d42f8b180f06221
James Smith <james.smith@djalbat.com>
2024-08-14 10:04:26 +0100

    Correctly exported the custom event types.

--- src/customEventTypes.js
+++ src/customEventTypes.js
@@ -4,3 +4,10 @@ export const BLUR_CUSTOM_EVENT_TYPE = "blur";
 export const FOCUS_CUSTOM_EVENT_TYPE = "focus";
 export const SCROLL_CUSTOM_EVENT_TYPE = "scroll";
 export const CHANGE_CUSTOM_EVENT_TYPE = "change";
+
+export default {
+  BLUR_CUSTOM_EVENT_TYPE,
+  FOCUS_CUSTOM_EVENT_TYPE,
+  SCROLL_CUSTOM_EVENT_TYPE,
+  CHANGE_CUSTOM_EVENT_TYPE
+};
